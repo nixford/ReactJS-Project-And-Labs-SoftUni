dea78735482eb2c28c810ba80ac04d0445787504
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-03-17 20:29:39 +0200

    Add dependencies for the app

--- ReactJS-Project/server/index.js
+++ ReactJS-Project/server/index.js
@@ -1,5 +1,10 @@
 import express from 'express'; // This is new sintaxis, which is enabled in package.json -> "type": "module", The old sintaxis: const express = require('express'); 
 import bodyParser from 'body-parser'; // Body-parser is the Node.js body parsing middleware. // It is responsible for parsing the incoming request bodies in a middleware before you handle it. // Body-parser parses your request and converts it into a format from which you can easily extract relevant information that you may need.
 import mongoose from 'mongoose';
-import cors from 'cors';
+import cors from 'cors'; // Enables cross-origin requests
 
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
