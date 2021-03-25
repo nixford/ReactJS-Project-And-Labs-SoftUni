10f574c896aec8cfbec144d301744a236b6955e0
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-03-25 16:59:32 +0200

    Add initialize react-redux

--- ReactJS-Project/client/src/App.js
+++ ReactJS-Project/client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-// import Posts from './components/Posts/Posts';
-// import Form from './components/Form/Form';
+import Properties from './components/Properties/Properties';
+import Form from './components/Form/Form';
 // import { getPosts } from './actions/posts';
-// import useStyles from './styles';
+import useStyles from './styles';
 // import memories from './images/memories.png';
 
 const App = () => {
@@ -27,7 +27,7 @@ const App = () => {
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Property setCurrentId={setCurrentId} />
+              <Properties setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
--- ReactJS-Project/client/src/index.js
+++ ReactJS-Project/client/src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Initialize redux - Provider provides access of the store from anywhere
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+import { formatMs } from '@material-ui/core';
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
