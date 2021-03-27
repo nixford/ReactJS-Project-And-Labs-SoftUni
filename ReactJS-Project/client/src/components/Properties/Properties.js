92fdf1b864a50df3837909b49857270aeea7ff47
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-03-27 18:59:44 +0200

    Altering components - connecting the form with db-properties

--- ReactJS-Project/client/src/components/Properties/Properties.js
+++ ReactJS-Project/client/src/components/Properties/Properties.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import Property from './Property/Property';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 
-const Properties = () => {
-    return (
-        <>
-            <h1>Properties</h1>
-            <Property />
-            <Property />
-        </>
+import Property from './Property/Property';
+import useStyles from '../../styles';
+
+
+const Properties = ({ setCurrentId }) => {
+  const properties = useSelector((state) => state.properties);
+  const classes = useStyles();
+
+
+  return (
+    !properties.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {properties.map((property) => (
+          <Grid key={property._id} item xs={12} sm={6} md={6}>
+            <Property property={property} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
     )
+  );
 };
 
 export default Properties;
